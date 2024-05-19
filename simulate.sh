57d0682b0782033a3bee60b8d1c8e68938fbbb2f
talkana <nataliaruteckaa@gmail.com>
2024-05-19 13:10:17 +0200

    typo fix

--- simulate.sh
+++ simulate.sh
@@ -13,7 +13,6 @@ for r in 5 10 15 20; do
   for l in 50 100 150 200; do
     for h in 10000000 500000; do
       curr_dir="${output_dir}/l${l}_r${r}_ILS_${height_to_name[$h]}"
-      mkdir -p "$curr_dir"
       network_commands+=("python3 network_sim.py -o '$curr_dir' -r $r -l $l -n $networks_per_parameter_set -d $displayed_trees_per_network -ht $h")
     done
   done
