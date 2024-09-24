615dc667c5f2b3f19e0e5dcef7aa6d713381935e
talkana <nataliaruteckaa@gmail.com>
2024-09-24 19:56:28 +0200

    fix typo

--- simulate_bijective.sh
+++ simulate_bijective.sh
@@ -8,7 +8,7 @@ simulate_networks() {
     for l in 50 100 150 200; do
       for h in 10000000 500000; do
         curr_dir="${output_dir}/l${l}_r${r}_ILS_${height_to_name[$h]}"
-        network_commands+=("python3 network_sim.py -o '$curr_dir' -r $r -l $l -n $networks_per_parameter_set -d $displayed_trees_per_network -ht $h")
+        network_commands+=("python3 network_sim.py -o '$curr_dir' -r $r -l $l -n $networks_per_parameter_set -d $displayed_trees_per_network_to_simulate -ht $h")
       done
     done
   done
@@ -24,7 +24,7 @@ simulate_trees_and_sequences() {
       tree_files=$(find "$output_dir" -type f -name "*displayed_trees")
       for tree_file in $tree_files; do
         reppath=$(dirname "$tree_file")
-        simphy_commands+=("simphy -o ${reppath} -sr ${tree_file} -rs ${displayed_trees_per_network} -I ${sp}")
+        simphy_commands+=("simphy -o ${reppath} -sr ${tree_file} -rs ${displayed_trees_per_network_to_simulate} -I ${sp}")
         indelible_commands+=("./INDELIble_wrapper.pl ${reppath} ${ip} ${seed} 1")
       done
     done
@@ -52,11 +52,11 @@ root_trees() {
   gtree_files=$(find "$output_dir" -type f -name "*_tree_ML")
   for gtree_file in $gtree_files; do
     # file cleanup
-    gtree_file_out = "${gtree_file%.newick}_cleaned.newick"
+    gtree_file_out="${gtree_file%.newick}_cleaned.newick"
     newick_cleanup "$gtree_file" "$gtree_file_out"
     dir_path=$(dirname "gtree_file")
     stree_file="$dir_path/s_tree.trees"
-    stree_file_out = "${stree_file%.trees}_cleaned.newick"
+    stree_file_out="${stree_file%.trees}_cleaned.newick"
     newick_cleanup "$stree_file" "$stree_file_out"
     # rooting
     urec_command="urec -G "{gtree_file_out}" -S "{stree_file_out}" -um -rmp1"
@@ -73,7 +73,8 @@ main() {
   output_dir="simulations_bijective"
   parameters_path="parameters"
   max_processes=20
-  displayed_trees_per_network=250
+  displayed_trees_per_network_required=250
+  displayed_trees_per_network_to_simulate=350 # added margin for cases where tree inferred from sequence is not bijective
   seed=42
   mkdir -p "$output_dir"
   simulate_networks
